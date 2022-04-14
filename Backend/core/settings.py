b97c927f1b64564af2fc57c088236f736adbd317
ailymas <ailymas2967@gmail.com>
2022-04-14 16:42:51 +0100

    crreate models and setup GraphQl

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
+    'orders',
+    'carts',
+    "graphene_django"
 ]
 
 MIDDLEWARE = [
@@ -74,11 +78,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':
+    {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'store', 
+        'USER': 'postgres', 
+        'PASSWORD': 'Kabyle2022',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
@@ -116,8 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+##GraphQl Root
+GRAPHENE = {
+    "SCHEMA": "core.schema.schema"
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# E-commerce-django-GraphQl-NextJS
+# E-commerce-django-GraphQl-NextJS  
+### initialize django project with:  
+`* django-admin startproject core .*`  
+### initialize a next application :  
+`* npx create-next-app e-commerce*`  
